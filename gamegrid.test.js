c83a98217979f108c0c1d1da58a3c64284fc2442
luisherreralg <lherreralg@outlook.es>
2022-10-16 21:24:16 +0200

    finished with gamegrid testing

---
+++ gamegrid.test.js
@@ -0,0 +1,70 @@
+import { createGridSave, createGameGrid } from './gamegrid.js';
+
+export const gameGridErrorControl = (Number) => {
+  if (typeof Number !== 'number') {
+    throw new Error(
+      'No se puede introducir un valor que no sea un tipo "number"'
+    );
+  }
+};
+
+describe('Given gameGridErrorControl function', () => {
+  test('If parameter is different from typeof "number" ', () => {
+    const notNumber = 'string';
+
+    expect(() => {
+      gameGridErrorControl(notNumber).toThrow();
+    });
+  });
+});
+
+describe('Given createGridSave function', () => {
+  test('If parameter is different from typeof "number" ', () => {
+    const notNumber = 'string';
+
+    expect(() => {
+      createGridSave(notNumber).toThrow();
+    });
+  });
+
+  test('When parameters are 2, the result must be an array with 2 arrays, each one must have 2 element', () => {
+    const expectedResult = [
+      ['', ''],
+      ['', ''],
+    ];
+
+    const result = createGridSave(2);
+
+    expect(result).toStrictEqual(expectedResult);
+  });
+});
+
+describe('Given createGameGrid funciton', () => {
+  test('If parameter is different from typeof "number" ', () => {
+    const notNumber = 'string';
+
+    expect(() => {
+      createGameGrid(notNumber).toThrow();
+    });
+  });
+
+  test('When parameters are 2, the result must be an array with 2 arrays, each one must have 2 elements', () => {
+    const checkArray = () => {
+      const arrTest = createGameGrid(2);
+      let counter = 0;
+      for (let i = 0; i < arrTest.length; i++) {
+        for (let j = 0; j < arrTest.length; j++) {
+          if (arrTest[i][j] !== undefined) {
+            counter++;
+          }
+        }
+      }
+      return counter;
+    };
+
+    const result = checkArray();
+    const expectedResult = 4;
+
+    expect(result).toStrictEqual(expectedResult);
+  });
+});
