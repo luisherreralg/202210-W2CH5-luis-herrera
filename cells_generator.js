c597a7b434ca7c4e3dd745228f4d132f5c17bf29
luisherreralg <lherreralg@outlook.es>
2022-10-16 18:38:41 +0200

    Reverted commit

--- cells_generator.js
+++
@@ -1,9 +0,0 @@
-export const deadCell = (emoteDead = '🌑') => {
-  const deadCell = emoteDead;
-  return deadCell;
-};
-
-export const aliveCell = (emoteAlive = '🧫') => {
-  const aliveCell = emoteAlive;
-  return aliveCell;
-};
--- index.js
+++
@@ -1,8 +0,0 @@
-import { runGame } from './game.js';
-import { gridNumberCalculator } from './grid_number_calculator.js';
-
-setInterval(() => {
-  // En el primer parámetro hay que introducir el tamaxo del grid. 10 = 10x10 (ejemplo)
-  // En los otros dos parámetros puedes introducir un emote para las células, primero las vivas y luego las muertas.
-  runGame(gridNumberCalculator(10), '🧫', '🌑');
-}, 1000);
